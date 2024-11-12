da708ed89c8b3424a4f2a9979168c8da73b6df97
Daniel Guimarães de Faria <danielguimaraesfaria@gmail.com>
2024-11-12 20:06:47 -0300

    Teste 1

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,20 @@
 #Criando um código para fibonacci
+
+A = 0
+B = 1
+C = 0
+
+lp = 200
+i  = 0
+
+print(A)
+print(B)
+while i < lp:
+    C = A + B
+    print(C)
+    A = B + C
+    print(A)
+    B = C + A
+    print(B)
+
+    i += 1
